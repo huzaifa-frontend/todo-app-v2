68838a8043c3dc07b39e403bc5f990f9f48fd062
Huzaifa Karim <karimhuzaifa590@gmail.com>
2024-10-30 15:17:13 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+let idCount = 1;
+
+document.getElementById("inp").addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        addTodo();
+    }
+});
+
+function addTodo() {
+    const todos = document.getElementById("todos");
+    const inpVal = document.getElementById("inp");
+
+    if (inpVal.value.trim() === "") return alert("Please enter a todo");
+
+    const element = document.createElement("div");
+    element.setAttribute("id", idCount);
+    element.setAttribute("class", "todo");
+
+    element.innerHTML = `
+        <p>${inpVal.value}</p>
+        <button class="deleteBtn" onclick="deleteTodo(${idCount})">Delete</button>
+        <button class="updateBtn" onclick="updateTodo(${idCount})">Update</button>
+    `;
+
+    todos.appendChild(element);
+    inpVal.value = "";
+    idCount++;
+}
+
+function deleteTodo(id) {
+    const deletedItem = document.getElementById(id);
+    deletedItem.remove();
+}
+
+function updateTodo(id) {
+    const inpVal = document.getElementById("inp");
+    const todoItem = document.getElementById(id).querySelector("p");
+
+    if (inpVal.value.trim() === "") return alert("Please enter a todo to update");
+
+    todoItem.textContent = inpVal.value;
+    inpVal.value = "";
+}
